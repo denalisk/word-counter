9b848f43995c4ba77443f683831923ba69928c1f
Sam Kirsch <samdkirsch2@gmail.com>
2017-02-07 23:25:51 -0500

    change the namespaces in HomeModules and Startup to match, alter the filepath linking Content\css\styles.css to the _Layout.cshtml, add the Razor block text to index.cshtml and forms.cshtml, and add skeleton content to HomeModules.cs

---
+++ HomeModules.cs
@@ -0,0 +1,12 @@
+using Nancy
+
+namespace MyApp
+{
+  public class HomeModule : NancyModule
+  {
+    public HomeModule()
+    {
+      // GETs and POSTs go HERE = _ =>
+    }
+  }
+}
--- Startup.cs
+++ Startup.cs
@@ -5,7 +5,7 @@ using Nancy;
 using Nancy.Owin;
 using Nancy.ViewEngines.Razor;
 
-namespace MyScope
+namespace MyApp
 {
   public class Startup
   {
