96deb32342ff1d693ec99754b05d8ea0786cd45a
Sam Kirsch <samdkirsch2@gmail.com>
2017-02-17 08:54:53 -0800

    create test for splitting the long string into a list of words

--- Tests/WordCounterTests.cs
+++ Tests/WordCounterTests.cs
@@ -10,13 +10,10 @@ namespace WordCounterApp.Objects
         {
             // Arrange
             string newLongString = "This is a string";
-
             // Act
             WordCounter newWordCounter = new WordCounter(newLongString);
-
             // Assert
             Assert.Equal(newLongString, newWordCounter.GetLongString());
-
         }
 
         [Fact]
@@ -26,13 +23,27 @@ namespace WordCounterApp.Objects
             string newLongString = "This is a string";
             string newTargetWord = "string";
             WordCounter newWordCounter = new WordCounter(newLongString);
-
             // Act
             newWordCounter.SetTargetWord(newTargetWord);
-
             // Assert
             Assert.Equal(newTargetWord, newWordCounter.GetTargetWord());
+        }
 
+        [Fact]
+        public void WordLister_Split_LongStringSplit()
+        {
+            // Arrange
+            string newLongString = "This is a string";
+            string newTargetWord = "string";
+            WordCounter newWordCounter = new WordCounter(newLongString);
+            List<string> resultsList = new List<string> {"This", "is", "a", "string"};
+            // Act
+            newWordCounter.StringSplitter()
+            // Assert
+            for (int i = 0; i < newWordCounter.GetWordList().Count; i++)
+            {
+                Assert.Equal(resultsList[i], newWordCounter.GetWordList()[i]);
+            }
         }
     }
 }
