7038455b3e48a3c0b806a2f254635218fe8b10f7
Sam Kirsch <samdkirsch2@gmail.com>
2017-02-17 11:24:49 -0800

    add more styling

--- Content/js/scripts.js
+++ Content/js/scripts.js
@@ -1,2 +1,18 @@
+var invalid = /\s/;
+
 $(function() {
+    $("#word-form").submit(function(event) {
+        event.preventDefault();
+    })
+    $("#form-submit").click(function() {
+        var targetWord = $("#target-word").val();
+        var longString = $("#long-string").val();
+        if (invalid.test(targetWord)) {
+            $("#invalid-entry-target").show();
+        } else if (longString.length == 0) {
+            $("#invalid-entry").show();
+        } else {
+            $("#word-form")[0].submit();
+        }
+    });
 });
